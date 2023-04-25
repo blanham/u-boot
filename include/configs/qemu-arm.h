1dfd3221d383067cdfe69d2a265b678c0292145f
Bryce Lanham <blanham@gmail.com>
2023-04-25 05:14:35 -0500

    Integrated Alex Graf ramfb code from the listserv
    This is very hacky, but it does work

--- include/configs/qemu-arm.h
+++ include/configs/qemu-arm.h
@@ -11,4 +11,22 @@
 
 /* For timer, QEMU emulates an ARMv7/ARMv8 architected timer */
 
+
+#ifdef CONFIG_VIDEO_RAMFB
+# define QEMU_STDOUT "serial,vidconsole"
+#else
+# define QEMU_STDOUT "serial"
+#endif
+
+#include <config_distro_bootcmd.h>
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+        "pxefile_addr_r=0x40300000\0" \
+        "kernel_addr_r=0x40400000\0" \
+        "ramdisk_addr_r=0x44000000\0" \
+       "stdin=serial\0" \
+       "stdout=" QEMU_STDOUT "\0" \
+       "stderr=" QEMU_STDOUT "\0" \
+        BOOTENV
+
 #endif /* __CONFIG_H */
